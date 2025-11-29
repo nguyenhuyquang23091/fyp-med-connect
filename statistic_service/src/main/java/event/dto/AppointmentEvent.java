ebdc8d4ae5c1393164843e8d4a75efdaf68bbfe0
Nguyen Huy Quang <nguyenhuyquang230904@gmail.com>
2025-11-29 11:15:42 +0700

    update statistic service

---
+++ statistic_service/src/main/java/event/dto/AppointmentEvent.java
@@ -0,0 +1,27 @@
+package dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppointmentEvent {
+    String eventId; // For idempotency
+    String appointmentId;
+    String userId;
+    String doctorId;
+    String specialty;
+    String consultationType;
+    String appointmentStatus;
+    BigDecimal price;
+    LocalDateTime appointmentDateTime;
+    Instant createdAt;
+}
